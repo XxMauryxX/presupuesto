be23a3762f5ba9245a8012517d71e34e5267e76e
XxMauryxX <59541736+XxMauryxX@users.noreply.github.com>
2020-06-28 21:30:34 -0500

    new

---
+++ build/precache-manifest.ba611cd465a81ce6638699a9d55c8251.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6114b2a1b336a498622c772762bbbb26",
+    "url": "/presupuesto/index.html"
+  },
+  {
+    "revision": "4492cb6be2552e5517d8",
+    "url": "/presupuesto/static/css/main.8e0335d0.chunk.css"
+  },
+  {
+    "revision": "35ff1e67a448da6e77b4",
+    "url": "/presupuesto/static/js/2.ce226546.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/presupuesto/static/js/2.ce226546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4492cb6be2552e5517d8",
+    "url": "/presupuesto/static/js/main.ffd145c9.chunk.js"
+  },
+  {
+    "revision": "3c4dce1ad28fc49a4a5d",
+    "url": "/presupuesto/static/js/runtime-main.ea810020.js"
+  }
+]);
